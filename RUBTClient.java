9a252fb2c5a74e155df180a34fb300fce89f2fb9
Moulindra Muchumari <mouleendhra@nwk-127-48.nwk.ruw.rutgers.edu>
2016-10-07 09:58:46 -0400

    Can open a torrent file and create a TorrentInfo Obj

---
+++ RUBTClient.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import GivenTools.*;
+
+public class RUBTClient {
+
+  /*
+    IO helper method - reads torrentfileName to return a byte array
+    @param torrentFileName string containing file name of the torrent file
+    @return torrentFileBytes byte array containing file
+  */
+  private static byte[] getByteArray(String torrentFileName) {
+    File torrentFile = new File(torrentFileName);
+
+    // Convert torrentFile into a byte array
+    byte[] torrentFileBytes = new byte[(int)torrentFile.length()];
+    DataInputStream dis;
+    try{
+      dis = new DataInputStream(new FileInputStream(torrentFile));
+      dis.readFully(torrentFileBytes);
+      dis.close(); // no need of dis -- close it
+      return torrentFileBytes;
+    } catch(FileNotFoundException fne) {
+      // Could not find file while trying to open a input stream of it
+      System.err.printf("File %s does not exist\n", torrentFileName);
+      return null;
+    } catch(IOException e) {
+      // Failed to readFully into the bytes array
+      System.err.println("Could not read file into byte array\n");
+      return null;
+    }
+  }
+
+  public static void main(String[] args){
+
+    if(args.length != 1) { // Check input args is correct usage
+      System.err.println("USAGE:java RUBTClient <torrent_file>");
+      return;
+    }
+
+    String torrentFileName = args[0];
+    byte[] torrentFileBytes = getByteArray(torrentFileName);
+    if(torrentFileBytes == null) return; // some error occured
+
+    try {
+      TorrentInfo tInfo = new TorrentInfo(torrentFileBytes);
+
+    } catch(BencodingException be) {
+      System.err.println(be);
+      return;
+    }
+
+
+
+  }
+}
